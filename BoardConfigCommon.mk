612682e7f54a1f3e6a276a3af3b1b6ce1841f018
Kilian von Pflugk <github@jumoog.io>
2018-12-10 12:31:27 +0100

    msm8994-common: Remove TARGET_USES_64_BIT_BINDER
    The TARGET_USES_64_BIT_BINDER option is deprecated and can be removed
    
    Change-Id: Ieab43c6baf17eff134a11312d8f8ee7d1b310d55

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -28,7 +28,6 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53.a57
 
-TARGET_USES_64_BIT_BINDER := true
 TARGET_CPU_SMP := true
 
 # Graphics

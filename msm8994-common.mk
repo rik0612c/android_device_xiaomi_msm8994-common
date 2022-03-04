a8ea2b34bbf683931c360eef2680e5b1350ca8a9
Etienne Ruffieux <eruffieux@google.com>
2022-03-04 13:31:43 +0000

    msm8994-common: Removed audio A2dp from device.mk
    Bug: 220306859
    Test: atest
    Tag: #feature
    Change-Id: I1af8461f8ad82afbb21b43dd6968e824ea89f5d6

--- msm8994-common.mk
+++ msm8994-common.mk
@@ -93,7 +93,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.2-impl \
-    audio.a2dp.default \
     audio.primary.$(BOARD_PLATFORM) \
     audio.r_submix.default \
     audio.usb.default \

37622550862ff7952038327be0251e559c82a0cf
rik0612c <qq872638399@gmail.com>
2023-03-31 23:29:38 +0800

    msm8994-common: Uprev SoundTrigger to 2.2

--- msm8994-common.mk
+++ msm8994-common.mk
@@ -92,7 +92,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@5.0-impl \
     android.hardware.audio.effect@5.0-impl \
-    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
     audio.primary.$(BOARD_PLATFORM) \
     audio.r_submix.default \

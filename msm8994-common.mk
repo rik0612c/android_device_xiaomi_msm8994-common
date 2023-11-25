bb8a3ae19e49bfb22300c6a3a6ca2f470e69464f
Nolen Johnson <johnsonnolen@gmail.com>
2023-11-25 13:46:42 -0500

    msm8994-common: Build 32 bit libminijail and libaudioroute
    * Not built by default anymore.
    
    Change-Id: I410314cb7130bc288112415937017eb5e5933b7b

--- msm8994-common.mk
+++ msm8994-common.mk
@@ -95,6 +95,7 @@ PRODUCT_PACKAGES += \
     audio.primary.$(BOARD_PLATFORM) \
     audio.r_submix.default \
     audio.usb.default \
+    libaudioroute \
     libaudio-resampler \
     libqcompostprocbundle \
     libqcomvisualizer \
@@ -191,6 +192,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.4-service.clearkey \
     libc2dcolorconvert \
+    libminijail:32 \
     libdrmclearkeyplugin \
     libmm-omxcore \
     libOmxAacEnc \
